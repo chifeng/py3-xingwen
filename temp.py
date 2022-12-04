5e20219558d1a3e78ea8465b12f01c0f91f8cf5c
Chifeng <chifeng@gmail.com>
2022-12-04 20:46:14 +0800

    a temp py file

---
+++ temp.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Hello world!")
+
+abcd = "Xingwen, 你好"
+
+print (abcd)
+
+print (abcd)
+
+abcd = "128512854815845"
+print (abcd)
+
+xyz="xingwu"
+print (xyz)
+
+#print = "print"
+#print (print)
+
+first = 5
+second = 3
+print (first+second)
+
+print (5+33333333333333+23242342352523523576767896787546765456787)
+
+first = first + 1
+print (first)
+
+print (abcd)
+
+print (5*3)
+print (5/3)
+print (5-3)
+print (3-5)
+print (5+3*(2-1)/2)
+
+a = 27
+print (a)
+b = float(a)
+print (b)
+c = float(abcd)
+print (c)
+
+print ("Please input:")
+x = input()
+x = int(x)
+x = float(x)
+
+print (x+100)
